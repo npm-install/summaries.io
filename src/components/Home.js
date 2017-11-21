252077dc369d17adce09fb3e5adf26628737c314
Allison von Bargen <gryffinzhao@gmail.com>
2017-11-21 16:22:11 -0500

    nav bar done for desktop

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-      <h1 className="welcome">Welcome! </h1>
+      <h1 className="welcome">Welcome back!</h1>
 
       <h2 className="welcome-title">Here is your summary for today: </h2>
 
