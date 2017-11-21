158f8e8ee4e00e3d9aa822bb70229d29a3a3a9a7
Adrien Lacquemant <verblodung@gmail.com>
2017-11-21 18:51:44 -0500

    feat(functions): initial makeSummaries function

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,38 @@ const functions = require('firebase-functions')
 
 const sendgrid = require('sendgrid')
 const client = sendgrid('YOUR_SG_API_KEY')
+const axios = require('axios')
+
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+
+const config = {
+  apiKey: 'AIzaSyAkc2x4ZAXRgOQHGkf6KGYyLTHz47PBqxs',
+  authDomain: 'summary-73ccc.firebaseapp.com',
+  databaseURL: 'https://summary-73ccc.firebaseio.com',
+  projectId: 'summary-73ccc',
+  storageBucket: 'summary-73ccc.appspot.com',
+  messagingSenderId: '494672399890'
+}
+
+function dateMaker() {
+  let date2 = Date().split(' ')
+  const monthToNum = {
+    Jan: 1,
+    Feb: 2,
+    Mar: 3,
+    Apr: 4,
+    May: 5,
+    Jun: 6,
+    Jul: 7,
+    Aug: 8,
+    Sep: 9,
+    Oct: 10,
+    Nov: 11,
+    Dec: 12
+  }
+  return `${date2[3]}-${monthToNum[date2[1]]}-${date2[2]} `
+}
 
 function parseBody(body) {
   const helper = sendgrid.mail
@@ -46,3 +78,63 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   )
 })
 
+exports.makeSummaries = functions.https.onRequest((request, response) => {
+  const { newsKey, sumKey } = require('./keys')
+
+  // First we retrieve the list of sources
+  const newsSource = 'bloomberg'
+
+  // What is today's date?
+  const date = dateMaker()
+
+  const newsUrl = `https://newsapi.org/v2/top-headlines?sources=${newsSource}&apiKey=${newsKey}`
+
+  let articles = []
+
+  return axios
+    .get(newsUrl)
+    .then(response => {
+      articles = response.data.articles.map(async article => {
+        const sumsObj = await axios.get(
+          `http://api.smmry.com/&SM_API_KEY=${sumKey}&&SM_LENGTH=2&SM_URL=${article.url}`
+        )
+        const updatedArticle = Object.assign({}, article, {
+          summary: sumsObj.data.sm_api_content || 'API MAXED OUT'
+        })
+        return updatedArticle
+      })
+      return Promise.all(articles)
+    })
+    .then(data => {
+      const batch = admin.firestore().batch()
+      const dayRef = admin
+        .firestore()
+        .collection('sources')
+        .doc(newsSource)
+        .collection('days')
+        .doc(date)
+        .collection('articles')
+
+      data.forEach(article => {
+        batch.set(dayRef.doc(article.title), { ...article })
+      })
+
+      // console.log(batch)
+
+      batch
+        .commit()
+        .then(console.log)
+        .catch(console.error)
+
+      // admin
+      //   .firestore()
+      //   .collection('sources')
+      //   .doc(newsSource)
+      //   .collection('days')
+      //   .doc(date)
+      //   .set({ ...data[0] })
+      //   .then(console.log)
+      response.json(data)
+    })
+    .catch(console.error)
+})
