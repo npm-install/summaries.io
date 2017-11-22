17fb2e6fce1523846c369a9b1b0646e29ad3028b
Allison Zhao <gryffinzhao@gmail.com>
2017-11-22 18:02:07 -0500

    Update Dashboard.js

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -11,14 +11,9 @@ export default class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      // value: '',
-      // message: '',
-      // timestamp: '',
       sources: [],
       expanded: false
     };
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -35,19 +30,6 @@ export default class Dashboard extends Component {
         this.setState({ sources: arr });
       });
   }
-
-  // handleChange(event) {
-  //   this.setState({ value: event.target.value })
-  // }
-
-  // handleSubmit(evt) {
-  //   evt.preventDefault();
-  //   var docRef = db.collection('user_input').doc('test_inputs')
-  //   docRef.set({
-  //     value: this.state.value,
-  //     timestamp: new Date()
-  //   });
-  // }
 
   render() {
     return (
@@ -81,24 +63,6 @@ export default class Dashboard extends Component {
           </div>
       </div>
 
-        {/* <div className="adrien-useless-stuff">
-          Dashboard. This is a protected route. You can only see this if you're
-          authed.
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input
-                type="text"
-                name="name"
-                value={this.state.value}
-                onChange={this.handleChange}
-              />
-            </label>
-            <input type="submit" value="Submit" />
-            <h2>{this.state.message}</h2>
-            Was posted on {this.state.timestamp.toString()}
-          </form>
-        </div> */}
       </div>
     );
   }
