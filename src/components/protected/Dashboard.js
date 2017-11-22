3f909cf0acaed9d6d1e5feaa4b73209d6f3b0539
Allison von Bargen <gryffinzhao@gmail.com>
2017-11-22 15:32:30 -0500

    finished importing sources on dashboard

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,73 +1,71 @@
-import React, { Component } from "react";
-import { db } from "../../config/constants";
-import { gadgets } from "../DumbyData";
-import Paper from "material-ui/Paper";
+import React, { Component } from 'react'
+import { db } from '../../config/constants'
+import Paper from 'material-ui/Paper'
 
 export default class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      value: "",
-      message: "",
-      timestamp: ""
+      // value: '',
+      // message: '',
+      // timestamp: '',
+      sources: []
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    // db.doc("user_input/test_inputs").onSnapshot(doc => {
-    //   const data = doc.data();
-    //   this.setState({
-    //     message: data.value || "",
-    //     timestamp: data.timestamp || ""
-    //   });
-    // });
+    let arr = []
     db
-      .collection('users/MSP2LuactJebx5sO4KXE/emails/2017-11-22/articles')
+      .collection('sources')
       .get()
       .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-          console.log(doc.id, ' => ', doc.data());
-        });
-      });
+        querySnapshot.forEach(function(source) {
+          arr.push(source.data())
+        })
+      })
+      .then(() => {
+        this.setState({sources: arr})
+      })
+
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+  // handleChange(event) {
+  //   this.setState({ value: event.target.value })
+  // }
 
-  handleSubmit(evt) {
-    evt.preventDefault();
-    var docRef = db.collection("user_input").doc("test_inputs");
-    docRef.set({
-      value: this.state.value,
-      timestamp: new Date()
-    });
-  }
+  // handleSubmit(evt) {
+  //   evt.preventDefault();
+  //   var docRef = db.collection('user_input').doc('test_inputs')
+  //   docRef.set({
+  //     value: this.state.value,
+  //     timestamp: new Date()
+  //   });
+  // }
 
   render() {
     return (
       <div>
         <div className="news-content">
-          {gadgets &&
-            gadgets.map(gad => (
-              <div key={gad.name}>
+          {this.state.sources.length &&
+            this.state.sources.map(source => (
+              <div key={source.id}>
                 <Paper
                   zDepth={2}
-                  style={{ marginRight: "1em", marginBottom: "1em" }}
+                  style={{ marginRight: '1em', marginBottom: '1em' }}
                   className="news-card"
                 >
                   <div className="news-grid">
-                    <img src={gad.image} alt={gad.name} className="news-logo" />
-                    <p className="news-name">{gad.name}</p>
+                    <img src={source.logo} alt={source.name} className="news-logo" />
+                    <p className="news-name">{source.name}</p>
                   </div>
                 </Paper>
               </div>
             ))}
         </div>
 
-        <div className="adrien-useless-stuff">
+        {/* <div className="adrien-useless-stuff">
           Dashboard. This is a protected route. You can only see this if you're
           authed.
           <form onSubmit={this.handleSubmit}>
@@ -84,7 +82,7 @@ export default class Dashboard extends Component {
             <h2>{this.state.message}</h2>
             Was posted on {this.state.timestamp.toString()}
           </form>
-        </div>
+        </div> */}
       </div>
     );
   }
