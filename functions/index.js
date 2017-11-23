01be9559a13c41cbe49847e0127b4eee5a5443d2
Eren Erdogan <ererdogan20@gmail.com>
2017-11-23 13:45:55 -0500

    split function into 2 functions for source for more stable writing

--- functions/index.js
+++ functions/index.js
@@ -64,25 +64,25 @@ exports.httpEmail = functions.https.onRequest((req, res) =>
     })
 );
 
-exports.makeSummaries = functions.https.onRequest((request, response) => {
+exports.makeSummaries1 = functions.https.onRequest((request, response) => {
   const { newsKey, sumKey } = require("./keys");
 
   // First we retrieve the list of sources
   const newsSources = [
-    "abc-news",
-    "al-jazeera-english",
-    "ars-technica",
-    "associated-press",
-    "axios",
-    "bleacher-report",
-    "bloomberg",
-    "business-insider",
-    "buzzfeed",
-    "cbs-news",
-    "cnbc",
-    "cnn",
-    "crypto-coins-news",
-    "engadget",
+    // "abc-news",
+    // "al-jazeera-english",
+    // "ars-technica",
+    // "associated-press",
+    // "axios",
+    // "bleacher-report",
+    // "bloomberg",
+    // "business-insider",
+    // "buzzfeed",
+    // "cbs-news",
+    // "cnbc",
+    // "cnn",
+    // "crypto-coins-news",
+    // "engadget",
     // "entertainment-weekly",
     // "espn"
     "fortune",
@@ -108,6 +108,137 @@ exports.makeSummaries = functions.https.onRequest((request, response) => {
     "usa-today",
     "vice-news",
     "wired"
+  ];
+  let articles = [];
+
+  // What is today's date?
+  const date = dateMaker();
+
+  Promise.map(newsSources, getSource)
+  .then(result => {
+    Promise.map(result, writeSource)
+      .then(_ => {
+        response.json("Successfully wrote all articles to the database, ayyy :)");
+      })
+      .catch((err) => {
+        console.error('Error writing to the database on a source,', err.message)
+        response.json('Error writing to the database on atleast one source, check logs')
+      })
+  })
+  .catch((err) => {
+    console.log('Error getting sources, ', err.message)
+    response.json('Error getting sources')
+  })
+
+
+  // Function definition to getSource
+  function getSource(newsSource) {
+    const newsUrl = `https://newsapi.org/v2/top-headlines?sources=${
+      newsSource
+    }&apiKey=${newsKey}`;
+
+    return axios.get(newsUrl).then(response => {
+      articles = response.data.articles.map(async article => {
+        const sumsObj = await axios
+          .get(
+            `http://api.smmry.com/&SM_API_KEY=${sumKey}&&SM_LENGTH=2&SM_URL=${
+              article.url
+            }`
+          )
+          .catch(console.error);
+        const updatedArticle = Object.assign({}, article, {
+          summary: sumsObj.data.sm_api_content || "API MAXED OUT"
+        });
+        return updatedArticle;
+      });
+      return Promise.all(articles);
+    });
+  }
+
+  // Write source to the database
+  function writeSource (data) {
+    const newsSource = data[0].source.id
+    const batch = admin.firestore().batch();
+    const dayRef = admin
+    .firestore()
+    .collection("sources")
+    .doc(newsSource)
+    .collection("days")
+    .doc(date)
+    .collection("articles");
+
+  data.forEach(article => {
+    batch.set(dayRef.doc(article.title), { ...article });
+  });
+
+  batch
+    .commit()
+    .then(() => {
+      console.log(
+        "added " +
+          articles.length +
+          " from " +
+          newsSource +
+          " to Firestore"
+      );
+    })
+    .catch(() => {
+      console.log(
+        "ERROR: Failed to write" +
+          articles.length +
+          " from " +
+          newsSource +
+          " to Firestore"
+      );
+    });
+
+  }
+});
+
+exports.makeSummaries2 = functions.https.onRequest((request, response) => {
+  const { newsKey, sumKey } = require("./keys");
+
+  // First we retrieve the list of sources
+  const newsSources = [
+    "abc-news",
+    "al-jazeera-english",
+    "ars-technica",
+    "associated-press",
+    "axios",
+    "bleacher-report",
+    "bloomberg",
+    "business-insider",
+    "buzzfeed",
+    "cbs-news",
+    "cnbc",
+    "cnn",
+    "crypto-coins-news",
+    "engadget"
+    // // "entertainment-weekly",
+    // // "espn"
+    // "fortune",
+    // // // "hacker-news",
+    // // "ign",
+    // // "mashable",
+    // // "msnbc",
+    // // "national-geographic",
+    // // "nbc-news",
+    // // "nfl-news",
+    // // "nhl-news",
+    // // "politico",
+    // // "polygon",
+    // "reuters",
+    // "techcrunch",
+    // "the-hill",
+    // "the-huffington-post",
+    // "the-new-york-times",
+    // "the-verge",
+    // "the-wall-street-journal",
+    // "the-washington-post",
+    // "time",
+    // "usa-today",
+    // "vice-news",
+    // "wired"
   ];
   let articles = [];
 
