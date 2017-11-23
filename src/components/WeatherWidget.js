77a9a56e6388ab772948529cdd3a9b76881dde77
Eren Erdogan <ererdogan20@gmail.com>
2017-11-22 20:06:55 -0500

    added weather comp

---
+++ src/components/WeatherWidget.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class WeatherWidget extends Component {
+  render() {
+    return (
+      <div>
+      <h1 className="welcome">Weather!</h1>
+
+      </div>
+    )
+  }
+}
