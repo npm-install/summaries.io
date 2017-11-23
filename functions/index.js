744fe1e12e1e7f2f61dafd79f01a024b57d91e3d
Eren Erdogan <ererdogan20@gmail.com>
2017-11-22 20:48:38 -0500

    modularized function for adding source

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,17 @@
-const functions = require('firebase-functions')
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-const sendgrid = require('sendgrid')
-const client = sendgrid('YOUR_SG_API_KEY')
-const axios = require('axios')
+const sendgrid = require("sendgrid");
+const client = sendgrid("YOUR_SG_API_KEY");
+const axios = require("axios");
 
-const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
 function dateMaker() {
-  let date2 = Date().split(' ')
+  let date2 = Date().split(" ");
   const monthToNum = {
     Jan: 1,
     Feb: 2,
@@ -25,98 +25,107 @@ function dateMaker() {
     Oct: 10,
     Nov: 11,
     Dec: 12
-  }
-  return `${date2[3]}-${monthToNum[date2[1]]}-${date2[2]} `
+  };
+  return `${date2[3]}-${monthToNum[date2[1]]}-${date2[2]} `;
 }
 
 function parseBody(body) {
-  const helper = sendgrid.mail
-  const fromEmail = new helper.Email(body.from)
-  const toEmail = new helper.Email(body.to)
-  const subject = body.subject
-  const content = new helper.Content('text/html', body.content)
-  const mail = new helper.Mail(fromEmail, subject, toEmail, content)
-  return mail.toJSON()
+  const helper = sendgrid.mail;
+  const fromEmail = new helper.Email(body.from);
+  const toEmail = new helper.Email(body.to);
+  const subject = body.subject;
+  const content = new helper.Content("text/html", body.content);
+  const mail = new helper.Mail(fromEmail, subject, toEmail, content);
+  return mail.toJSON();
 }
 
 exports.httpEmail = functions.https.onRequest((req, res) =>
   Promise.resolve()
     .then(_ => {
-      if (req.method !== 'POST') {
-        const error = new Error('Only POST requests are accepted')
-        error.code = 405
-        throw error
+      if (req.method !== "POST") {
+        const error = new Error("Only POST requests are accepted");
+        error.code = 405;
+        throw error;
       }
 
       const request = client.emptyRequest({
-        method: 'POST',
-        path: '/v3/mail/send',
+        method: "POST",
+        path: "/v3/mail/send",
         body: parseBody(req.body)
-      })
+      });
 
-      return client.API(request)
+      return client.API(request);
     })
     .then(response => (response.body ? res.send(response.body) : res.end()))
     .catch(err => {
-      console.error(err)
-      return Promise.reject(err)
+      console.error(err);
+      return Promise.reject(err);
     })
-)
+);
 
 exports.makeSummaries = functions.https.onRequest((request, response) => {
-  const { newsKey, sumKey } = require('./keys')
+  const { newsKey, sumKey } = require("./keys")
 
-  // // First we retrieve the list of sources
+  // First we retrieve the list of sources
   const newsSources = [
-    // "abc-news",
-    // "al-jazeera-english",
-    // "ars-technica",
-    // "associated-press",
-    // "axios",
-    // "bleacher-report",
-    // "bloomberg",
-    // "business-insider",
-    // "buzzfeed",
-    // "cbs-news",
-    // "cnbc",
-    // "cnn",
-    // "crypto-coins-news",
-    // "engadget",
-    // "entertainment-weekly",
-    // "espn",
-    // "fortune",
-    // "hacker-news",
-    // "ign",
-    // "mashable"
-    'msnbc',
-    'national-geographic',
-    'nbc-news',
-    'nfl-news',
-    'nhl-news',
-    'politico',
-    'polygon',
+    "abc-news",
+    "al-jazeera-english",
+    "ars-technica",
+    "associated-press",
+    "axios",
+    "bleacher-report",
+    "bloomberg",
+    "business-insider",
+    "buzzfeed",
+    "cbs-news",
+    "cnbc",
+    "cnn",
+    "crypto-coins-news",
+    "engadget",
+    "entertainment-weekly",
+    "espn",
+    "fortune",
+    "hacker-news",
+    "ign",
+    "mashable",
+    "msnbc",
+    "national-geographic",
+    "nbc-news",
+    "nfl-news",
+    "nhl-news",
+    "politico",
+    "polygon",
     // 'reddit-r-all',
-    'reuters',
-    'techcrunch',
-    'the-hill',
-    'the-huffington-post',
-    'the-new-york-times',
-    'the-verge',
-    'the-wall-street-journal',
-    'the-washington-post',
-    'time',
-    'usa-today',
-    'vice-news',
-    'wired'
-  ]
+    "reuters",
+    "techcrunch",
+    "the-hill",
+    "the-huffington-post",
+    "the-new-york-times",
+    "the-verge",
+    "the-wall-street-journal",
+    "the-washington-post",
+    "time",
+    "usa-today",
+    "vice-news",
+    "wired"
+  ];
 
   // What is today's date?
   const date = dateMaker()
 
-  newsSources.forEach(newsSource => {
-    const newsUrl = `https://newsapi.org/v2/top-headlines?sources=${newsSource}&apiKey=${newsKey}`
+  // newsSources.forEach(source => {
+  //   writeSource(source)
+  // })
 
-    let articles = []
+  writeSource('the-washington-post')
+
+  // Function definition to write to database
+  function writeSource(newsSource) {
+    const newsUrl = `https://newsapi.org/v2/top-headlines?sources=${
+      newsSource
+    }&apiKey=${newsKey}`;
+
+    let articles = [];
 
     axios
       .get(newsUrl)
@@ -124,82 +133,84 @@ exports.makeSummaries = functions.https.onRequest((request, response) => {
         articles = response.data.articles.map(async article => {
           const sumsObj = await axios
             .get(
-              `http://api.smmry.com/&SM_API_KEY=${sumKey}&&SM_LENGTH=2&SM_URL=${article.url}`
+              `http://api.smmry.com/&SM_API_KEY=${sumKey}&&SM_LENGTH=2&SM_URL=${
+                article.url
+              }`
             )
-            .catch(console.error)
+            .catch(console.error);
           const updatedArticle = Object.assign({}, article, {
-            summary: sumsObj.data.sm_api_content || 'API MAXED OUT'
-          })
-          return updatedArticle
-        })
-        return Promise.all(articles)
+            summary: sumsObj.data.sm_api_content || "API MAXED OUT"
+          });
+          return updatedArticle;
+        });
+        return Promise.all(articles);
       })
       .then(data => {
-        const batch = admin.firestore().batch()
+        const batch = admin.firestore().batch();
         const dayRef = admin
           .firestore()
-          .collection('sources')
+          .collection("sources")
           .doc(newsSource)
-          .collection('days')
+          .collection("days")
           .doc(date)
-          .collection('articles')
+          .collection("articles");
 
         data.forEach(article => {
-          batch.set(dayRef.doc(article.title), { ...article })
-        })
+          batch.set(dayRef.doc(article.title), { ...article });
+        });
 
         batch
           .commit()
           .then(() => {
             console.log(
-              'added ' +
+              "added " +
                 articles.length +
-                ' from ' +
+                " from " +
                 newsSource +
-                ' to Firestore'
-            )
+                " to Firestore"
+            );
           })
-          .catch(console.error)
+          .catch(console.error);
       })
-      .catch(console.error)
-  })
+      .catch(console.error);
+  }
 
-    //   batch
-    //     .commit()
-    //     // .then(console.log)
-    //     .then(() => {
-    //       response.json(data)
-    //     })
-    //     .catch(console.error)
-    // .catch(console.error)
-    response.json("Check console.logs, function running")
-})
+  //   batch
+  //     .commit()
+  //     // .then(console.log)
+  //     .then(() => {
+  //       response.json(data)
+  //     })
+  //     .catch(console.error)
+  // .catch(console.error)
+  response.json("Check console.logs, function running");
+});
 
 exports.makeEmails = functions.https.onRequest((request, response) => {
   const rec = admin
     .firestore()
-    .collection('users')
+    .collection("users")
     // Here add a where query to filter by requested time
     // For now, let's generate Adrien's email
-    .where('email', '==', 'q@q.com')
+    .where("email", "==", "q@q.com")
     .get()
     .then(function(users) {
       users.forEach(function(user) {
         admin
           .firestore()
-          .collection('users')
+          .collection("users")
           .doc(user.id)
-          .collection('subscriptions')
+          .collection("subscriptions")
           .get()
           .then(subscriptions => {
             subscriptions.forEach(subscription => {
-              console.log(subscription.id)
-            })
+              console.log(subscription.id);
+            });
           })
-          .catch(console.error)
-      })
+          .catch(console.error);
+      });
     })
-    .catch(console.error)
+    .catch(console.error);
 
-  response.send('hello')
-})
+  response.send("hello");
+});
