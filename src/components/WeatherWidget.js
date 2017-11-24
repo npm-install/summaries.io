285179e95bb0770ab3f4dc96a0db1fb0cc80cc33
Eren Erdogan <ererdogan20@gmail.com>
2017-11-24 16:51:41 -0500

    zip code input field done

--- src/components/WeatherWidget.js
+++ src/components/WeatherWidget.js
@@ -10,42 +10,67 @@ export default class WeatherWidget extends Component {
       zipCode: '',
       weatherJSON: {}
     };
+
+    this.submitHandler = this.submitHandler.bind(this)
   }
 
   componentDidMount() {
-    const weatherKey = "56f2b6fcede722ad8a463396763c6270";
-    console.log(weatherKey);
-    axios
-      .get(
-      `http://api.openweathermap.org/data/2.5/weather?zip=${
-      this.state.zipCode
-      },us&appid=${weatherKey}`
-      )
-      .then(res => {
-        this.setState({ weatherJSON: res.data });
-      });
+    const weatherKey = '56f2b6fcede722ad8a463396763c6270'
+    // console.log(weatherKey)
+
+    if (this.state.zipCode) {
+      axios
+        .get(
+        `http://api.openweathermap.org/data/2.5/weather?zip=${
+        this.state.zipCode
+        },us&appid=${weatherKey}`
+        )
+        .then(res => {
+          console.log(res.data)
+          // this.setState({ weatherJSON: res.data });
+        })
+        .catch(console.error)
+    }
+  }
+
+  submitHandler(event) {
+    event.preventDefault()
+    let zip = event.target.zip.value;
+
+    while (zip.length < 5) zip = '0' + zip;
+
+    this.setState({ zipCode: zip })
   }
 
   render() {
-    console.log(this.state.weatherJSON);
+    console.log(this.state.zipCode)
 
     if (!this.state.zipCode) {
       return (
         <div>
-          <h1>Enter your zip code to get weather!</h1>
           <Paper zDepth={2} className="article-card">
-            <form>
-              <label htmlFor="zip">Zip Code:</label>
-              <input name="zip" placeholder="10001" />
-              <button type="submit" className="btn btn-primary">
-                Submit
+            <div className="zipcode-input">
+              <h3>Enter your zip code to get weather!</h3>
+              <form onSubmit={this.submitHandler}>
+                <label htmlFor="zip">Zip Code:</label>
+                <input name="zip" placeholder="10001" type="number" step="1" min="00000" max="99999" />
+                <button type="submit" className="btn btn-primary">
+                  Submit
               </button>
-            </form>
+              </form>
+            </div>
           </Paper>
         </div>
       );
     }
-
+    else if (Object.keys(this.state.weatherJSON).length === 0) {
+      return (
+        <Paper zDepth={2} className="article-card">
+          <div className="zipcode-input">
+            <div>Loading your weather</div>
+          </div>
+        </Paper>)
+    }
     return (
       <div>
         <h1>Weather for {this.state.zipCode}</h1>
