526bcfb1048121fe5b8b38bbabe581b0b2aacb6c
Eren Erdogan <ererdogan20@gmail.com>
2017-11-25 16:12:54 -0500

    MVP of weather widget

--- src/components/WeatherWidget.js
+++ src/components/WeatherWidget.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-import axios from 'axios';
 import ReactLoading from 'react-loading';
+import { setTimeout } from 'core-js/library/web/timers';
+import zipcodes from 'zipcodes'
+import DarkSkyApi from 'dark-sky-api';
+import Skycons from 'react-skycons'
 // const { weatherKey } = require('../functions/keys.js');
+let count = 1;
 
 export default class WeatherWidget extends Component {
   constructor(props) {
     super(props);
     this.state = {
       zipCode: '',
-      weatherJSON: {}
+      weatherJSON: {},
+      city: ''
     };
 
     this.submitHandler = this.submitHandler.bind(this)
   }
 
-  componentDidMount() {
-    const weatherKey = '56f2b6fcede722ad8a463396763c6270'
-    console.log(weatherKey)
 
-    if (this.state.zipCode.length === 5) {
-      console.log(weatherKey)
-      axios
-        .get(
-        `http://api.openweathermap.org/data/2.5/weather?zip=${
-        this.state.zipCode
-        },us&appid=${weatherKey}`
+  getData(zip) {
+    const weatherKey = '3802941ad5b8c716614249ea1cf918b8'
+
+    DarkSkyApi.apiKey = weatherKey;
+
+    const location = zipcodes.lookup(zip)
+
+    const position = {
+      latitude: location.latitude,
+      longitude: location.longitude
+    };
+
+    DarkSkyApi.loadCurrent(position)
+      .then(result => {
+        this.setState(
+          {
+            weatherJSON: result,
+            city: location.city
+          }
         )
-        .then(res => {
-          console.log(res.data)
-          // this.setState({ weatherJSON: res.data });
-        })
-        .catch(console.error)
-    }
+      })
+      .catch(console.error);
+
   }
 
   submitHandler(event) {
@@ -41,16 +52,24 @@ export default class WeatherWidget extends Component {
 
     while (zip.length < 5) zip = '0' + zip;
 
-    this.setState({ zipCode: zip })
-    this.componentDidMount()
+    this.setState({ zipCode: zip, weatherJSON: {} })
+
+    setTimeout(() => {
+      this.getData(zip)
+
+    }, 2000)
   }
 
   render() {
+
+    console.log('Component rendered', count++, 'time(s)')
     console.log(this.state)
+    // if (count === 2) this.getData()
 
     if (!this.state.zipCode) {
       return (
         <div>
+        <Skycons color='white' icon='CLEAR_NIGHT' />
           <Paper zDepth={2} className="article-card">
             <div className="zipcode-input">
               <h3>Enter your zip code to get weather!</h3>
@@ -72,16 +91,18 @@ export default class WeatherWidget extends Component {
           <div className="loading-weather">
             <ReactLoading type="spinningBubbles" color="#444" />
           </div>
-        </Paper>)
+        </Paper>
+      )
     }
     const weather = this.state.weatherJSON;
     console.log(weather)
     return (
       <div>
-        <h1>Weather for {this.state.zipCode}</h1>
         <Paper zDepth={2} className="article-card">
+          <h1>Weather for {this.state.city}</h1>
           <div>
-            <p>Heres ya weather fam</p>
+            <p>Temperature: {Math.floor(weather.temperature)} Degrees</p>
+
           </div>
         </Paper>
       </div>
