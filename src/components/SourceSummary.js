673b411576992ae38602cd994f5ae308c60afc18
Adrien <verblodung@gmail.com>
2017-11-26 15:23:32 -0500

    fix(summary): adding componentDIdMount

--- src/components/SourceSummary.js
+++ src/components/SourceSummary.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
-import Article from './Article';
-import Paper from 'material-ui/Paper';
-import {NYT, IGN, TechCrunch} from './DumbyData';
+import React, { Component } from 'react'
+import Article from './Article'
+import Paper from 'material-ui/Paper'
+import { NYT, IGN, TechCrunch } from './DumbyData'
 
 export default class SourceSummary extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      articles: {
-        newyorktimes: NYT,
-        ignnews: IGN,
-        tc: TechCrunch
-      }
-    };
+      articles: {}
+    }
+  }
+
+  componentDidMount() {
+    const articles = {
+      newyorktimes: NYT,
+      ignnews: IGN,
+      tc: TechCrunch
+    }
+    this.setState({ articles: articles })
   }
 
   render() {
+    console.log(this.state.articles)
     return (
       <div className="source-summary">
-        {
-          this.state.articles.newyorktimes.length && Object.keys(this.state.articles).map(key => (
+        {this.state.articles.newyorktimes &&
+          Object.keys(this.state.articles).map(key => (
             <div key={key} className="each-source">
               <div className="source-header">
                 <img
-                  src={
-                    this.state.articles[key][0].source.logo
-                  }
+                  src={this.state.articles[key][0].source.logo}
                   className="sourcelogo"
                   alt="logo"
                 />
@@ -34,20 +38,21 @@ export default class SourceSummary extends Component {
                 </h3>
               </div>
 
-            <div className="source-content">
-                {
-                  this.state.articles[key].map(article => (
-                    <Paper zDepth={2} key={article.title} className="article-card">
-                      <div className="each-article">
-                        <Article article={article} />
-                      </div>
-                    </Paper>
-                  ))
-                }
+              <div className="source-content">
+                {this.state.articles[key].map(article => (
+                  <Paper
+                    zDepth={2}
+                    key={article.title}
+                    className="article-card"
+                  >
+                    <div className="each-article">
+                      <Article article={article} />
+                    </div>
+                  </Paper>
+                ))}
+              </div>
             </div>
-          </div>
-          ))
-        }
+          ))}
       </div>
     )
   }
