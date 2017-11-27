9c941b3aa24d2b058622f8b9dd33c8877a8a2c1b
Allison von Bargen <gryffinzhao@gmail.com>
2017-11-26 20:17:13 -0500

    basic UI for user profile page

--- src/components/User.js
+++ src/components/User.js
@@ -1,32 +1,66 @@
-import React from 'react'
-import TextField from 'material-ui/TextField'
-import Paper from 'material-ui/Paper'
-import { RaisedButton } from 'material-ui'
+import React from "react";
+import { RaisedButton } from "material-ui";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
 
 export default function User(props) {
-  const email = props.user.email
+  const email = props.user.email;
+
   return (
-    <div className="user-container">
-      <div>
-        <Paper circle={true} zDepth={2} />
-      </div>
-      <span>
-        <div>
-          <TextField hintText={`First Name`} />
-        </div>
-        <div>
-          <TextField hintText={`Last Name`} />
-        </div>
-        <div>
-          <TextField hintText={`Email Address`} defaultValue={email} />
-        </div>
+    <div>
+      <h2 className="welcome-message">Welcome, {email}</h2>
+
+      <div className="user-select">
         <RaisedButton
-          label="Save"
-          primary={true}
-          type="submit"
+          label="Reset Password"
+          secondary={true}
+          className="user-select-btn"
         />
-        <div>{JSON.stringify(props.user)}</div>
-      </span>
+        <RaisedButton
+          label="Delete Account"
+          secondary={true}
+          className="user-select-btn"
+        />
+        <SelectField
+          floatingLabelText="Set Email Frequency"
+          className="user-select-btn"
+        >
+          <MenuItem value={1} primaryText="Never" />
+          <MenuItem value={2} primaryText="Every Night" />
+          <MenuItem value={3} primaryText="Weeknights" />
+          <MenuItem value={4} primaryText="Weekends" />
+          <MenuItem value={5} primaryText="Weekly" />
+        </SelectField>
+        <SelectField
+          floatingLabelText="Set Email Sent Time"
+          className="user-select-btn"
+        >
+          <MenuItem value={1} primaryText="00:00" />
+          <MenuItem value={2} primaryText="01:00" />
+          <MenuItem value={3} primaryText="02:00" />
+          <MenuItem value={4} primaryText="03:00" />
+          <MenuItem value={5} primaryText="04:00" />
+          <MenuItem value={5} primaryText="05:00" />
+          <MenuItem value={5} primaryText="06:00" />
+          <MenuItem value={5} primaryText="07:00" />
+          <MenuItem value={5} primaryText="08:00" />
+          <MenuItem value={5} primaryText="09:00" />
+          <MenuItem value={5} primaryText="10:00" />
+          <MenuItem value={5} primaryText="11:00" />
+          <MenuItem value={5} primaryText="12:00" />
+          <MenuItem value={5} primaryText="13:00" />
+          <MenuItem value={5} primaryText="14:00" />
+          <MenuItem value={5} primaryText="15:00" />
+          <MenuItem value={5} primaryText="16:00" />
+          <MenuItem value={5} primaryText="17:00" />
+          <MenuItem value={5} primaryText="18:00" />
+          <MenuItem value={5} primaryText="19:00" />
+          <MenuItem value={5} primaryText="20:00" />
+          <MenuItem value={5} primaryText="21:00" />
+          <MenuItem value={5} primaryText="22:00" />
+          <MenuItem value={5} primaryText="23:00" />
+        </SelectField>
+      </div>
     </div>
-  )
+  );
 }
