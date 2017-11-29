4a2e02bc6b92ab9c6b7000f0872b1df8c13d851a
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-11-29 16:08:14 -0500

    comment out async

--- functions/index.js
+++ functions/index.js
@@ -32,101 +32,101 @@ function dateMaker() {
 //     .catch(err => console.log('error: ', err))
 // })
 
-function makeEmail(user) {
-  let userSource = []
-  admin
-    .firestore()
-    .collection('users')
-    .doc(user)
-    .collection('emails')
-    .doc(dateMaker())
-    .get()
-    .then(doc => {
-      userSource = Object.keys(doc.data())
-    })
-    .then(() => {
-      const promises = userSource.map(async source => {
-        const articles = await admin
-          .firestore()
-          .collection('users')
-          .doc(user)
-          .collection('emails')
-          .doc(dateMaker())
-          .collection(source)
-          .get()
-          .then(snapshot => {
-            const artFromSource = []
-            snapshot.forEach(doc => {
-              artFromSource.push(doc.data())
-            })
-            return artFromSource
-          })
+// function makeEmail(user) {
+//   let userSource = []
+//   admin
+//     .firestore()
+//     .collection('users')
+//     .doc(user)
+//     .collection('emails')
+//     .doc(dateMaker())
+//     .get()
+//     .then(doc => {
+//       userSource = Object.keys(doc.data())
+//     })
+//     .then(() => {
+//       const promises = userSource.map(async source => {
+//         const articles = await admin
+//           .firestore()
+//           .collection('users')
+//           .doc(user)
+//           .collection('emails')
+//           .doc(dateMaker())
+//           .collection(source)
+//           .get()
+//           .then(snapshot => {
+//             const artFromSource = []
+//             snapshot.forEach(doc => {
+//               artFromSource.push(doc.data())
+//             })
+//             return artFromSource
+//           })
 
-        return { [source]: articles }
-      })
+//         return { [source]: articles }
+//       })
 
-      return Promise.all(promises).then(articleObjects => Object.assign({}, ...articleObjects))
-    })
-    .then(arr => {
-      const allSources = Object.keys(arr).map(key => {
-        const header = `<h2 style="margin-top: 1.5em;">${arr[key][0].source.name}</h2>`
-        const content = arr[key]
-          .map(
-            article =>
-              `<div style="border-radius: 20px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); width: 80vw; padding: 1.5em; margin-bottom: 1em;"><a href=${
-                article.url
-              } style="font-size: 1.2em;">${article.title}</a>
-              <p style="font-size: 1em;">${article.summary}</p></div>`
-          )
-          .join('')
-        const audioContent = `Here are your summaries from ${arr[key][0].source.name}.  
-        ${arr[key]
-          .map(article => `You are now listening to ${article.title}. ${article.summary} `)
-          .join('')}`
-        const body = header + content
-        return { body, audioContent }
-      })
-      const logo = `<div style="text-align: center;"><img src="https://i.imgur.com/RgjJeBb.png" alt="email-logo" style="width: 50vw; height: auto;"></div>`
-      const welcome = '<h2 style="font-size: 1.8em;">Welcome to your daily summary</h2>'
+//       return Promise.all(promises).then(articleObjects => Object.assign({}, ...articleObjects))
+//     })
+//     .then(arr => {
+//       const allSources = Object.keys(arr).map(key => {
+//         const header = `<h2 style="margin-top: 1.5em;">${arr[key][0].source.name}</h2>`
+//         const content = arr[key]
+//           .map(
+//             article =>
+//               `<div style="border-radius: 20px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); width: 80vw; padding: 1.5em; margin-bottom: 1em;"><a href=${
+//                 article.url
+//               } style="font-size: 1.2em;">${article.title}</a>
+//               <p style="font-size: 1em;">${article.summary}</p></div>`
+//           )
+//           .join('')
+//         const audioContent = `Here are your summaries from ${arr[key][0].source.name}.
+//         ${arr[key]
+//           .map(article => `You are now listening to ${article.title}. ${article.summary} `)
+//           .join('')}`
+//         const body = header + content
+//         return { body, audioContent }
+//       })
+//       const logo = `<div style="text-align: center;"><img src="https://i.imgur.com/RgjJeBb.png" alt="email-logo" style="width: 50vw; height: auto;"></div>`
+//       const welcome = '<h2 style="font-size: 1.8em;">Welcome to your daily summary</h2>'
 
-      const html = `<html>${logo}${welcome} <div style="line-height: 1.5em"> ${allSources
-        .map(source => source.body)
-        .join('')} </div></html>`
-      const audio = `Welcome to Summaries dot I O. ${allSources
-        .map(source => source.audioContent)
-        .join('')}. Those were your summaries for today. See you tomorrow!`
+//       const html = `<html>${logo}${welcome} <div style="line-height: 1.5em"> ${allSources
+//         .map(source => source.body)
+//         .join('')} </div></html>`
+//       const audio = `Welcome to Summaries dot I O. ${allSources
+//         .map(source => source.audioContent)
+//         .join('')}. Those were your summaries for today. See you tomorrow!`
 
-      return { html, audio }
-    })
-    .then(({ html, audio }) => {
-      speech(user, audio)
-      return sendEmail(user, html)
-    })
-    .catch(err => console.log('error: ', err))
-}
+//       return { html, audio }
+//     })
+//     .then(({ html, audio }) => {
+//       speech(user, audio)
+//       return sendEmail(user, html)
+//     })
+//     .catch(err => console.log('error: ', err))
+// }
 
-function sendEmail(user, html) {
-  const { gmail } = require('./keys')
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'summariesio@gmail.com',
-      pass: gmail
-    }
-  })
+// function sendEmail(user, html) {
+//   const { gmail } = require('./keys')
+//   var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: 'summariesio@gmail.com',
+//       pass: gmail
+//     }
+//   })
 
-  const mailOptions = {
-    from: '⚡ summaries.io ⚡ <your@summaries.io>',
-    to: user,
-    subject: 'Your daily summaries',
-    html: html
-  }
+//   const mailOptions = {
+//     from: '⚡ summaries.io ⚡ <your@summaries.io>',
+//     to: user,
+//     subject: 'Your daily summaries',
+//     html: html
+//   }
 
-  transporter.sendMail(mailOptions, function(err, info) {
-    if (err) return err
-    else return info
-  })
-}
+//   transporter.sendMail(mailOptions, function(err, info) {
+//     if (err) return err
+//     else return info
+//   })
+// }
 
 exports.podcast = functions.https.onRequest((request, response) => {
   /* lets create an rss feed */
@@ -378,136 +378,136 @@ function speech(email, audioString) {
 //   }
 // })
 
-exports.makeEmails = functions.https.onRequest((request, response) => {
-  const today = dateMaker()
+// exports.makeEmails = functions.https.onRequest((request, response) => {
+//   const today = dateMaker()
 
-  admin
-    .firestore()
-    .collection('users')
-    .get()
-    .then(function(users) {
-      const batch = admin.firestore().batch()
-      users.forEach(function(user) {
-        admin
-          .firestore()
-          .collection('users')
-          .doc(user.id)
-          .collection('subscriptions')
-          .get()
-          .then(subscriptions => {
-            subscriptions.forEach(subscription => {
-              const sub = subscription.id
-              batch.set(
-                admin
-                  .firestore()
-                  .collection('users')
-                  .doc(user.id)
-                  .collection('emails')
-                  .doc(today),
-                { [sub]: true },
-                { merge: true }
-              )
-              admin
-                .firestore()
-                .collection('sources')
-                .doc(subscription.id)
-                .collection('days')
-                .doc(today)
-                .collection('articles')
-                .get()
-                .then(articles => {
-                  articles.forEach(article => {
-                    const articleContent = article.data()
-                    batch.set(
-                      admin
-                        .firestore()
-                        .collection('users')
-                        .doc(user.id)
-                        .collection('emails')
-                        .doc(today)
-                        .collection(subscription.id)
-                        .doc(article.id),
-                      { ...articleContent }
-                    )
-                  })
-                })
-                .then(() => {
-                  batch
-                    .commit()
-                    .then(console.log)
-                    .catch(console.error)
-                })
-                .catch(console.error)
-            })
-          })
-          .catch(console.error)
-      })
-    })
-    .catch(console.error)
-  response.send('emails are being created')
-})
+//   admin
+//     .firestore()
+//     .collection('users')
+//     .get()
+//     .then(function(users) {
+//       const batch = admin.firestore().batch()
+//       users.forEach(function(user) {
+//         admin
+//           .firestore()
+//           .collection('users')
+//           .doc(user.id)
+//           .collection('subscriptions')
+//           .get()
+//           .then(subscriptions => {
+//             subscriptions.forEach(subscription => {
+//               const sub = subscription.id
+//               batch.set(
+//                 admin
+//                   .firestore()
+//                   .collection('users')
+//                   .doc(user.id)
+//                   .collection('emails')
+//                   .doc(today),
+//                 { [sub]: true },
+//                 { merge: true }
+//               )
+//               admin
+//                 .firestore()
+//                 .collection('sources')
+//                 .doc(subscription.id)
+//                 .collection('days')
+//                 .doc(today)
+//                 .collection('articles')
+//                 .get()
+//                 .then(articles => {
+//                   articles.forEach(article => {
+//                     const articleContent = article.data()
+//                     batch.set(
+//                       admin
+//                         .firestore()
+//                         .collection('users')
+//                         .doc(user.id)
+//                         .collection('emails')
+//                         .doc(today)
+//                         .collection(subscription.id)
+//                         .doc(article.id),
+//                       { ...articleContent }
+//                     )
+//                   })
+//                 })
+//                 .then(() => {
+//                   batch
+//                     .commit()
+//                     .then(console.log)
+//                     .catch(console.error)
+//                 })
+//                 .catch(console.error)
+//             })
+//           })
+//           .catch(console.error)
+//       })
+//     })
+//     .catch(console.error)
+//   response.send('emails are being created')
+// })
 
-exports.getWeather = functions.https.onRequest((request, response) => {
-  const { weatherKey } = require('./keys')
+// exports.getWeather = functions.https.onRequest((request, response) => {
+//   const { weatherKey } = require('./keys')
 
-  // Initialize
-  const Forecast = require('forecast')
+//   // Initialize
+//   const Forecast = require('forecast')
 
-  const forecast = new Forecast({
-    service: 'darksky',
-    key: weatherKey,
-    units: 'fahrenheit'
-  })
+//   const forecast = new Forecast({
+//     service: 'darksky',
+//     key: weatherKey,
+//     units: 'fahrenheit'
+//   })
 
-  // Get users from db
-  admin
-    .firestore()
-    .collection('users')
-    .get()
-    .then(users => {
-      // get all zipCodes from the users in a set to avoid duplicates
-      const zipCodesSet = new Set()
+//   // Get users from db
+//   admin
+//     .firestore()
+//     .collection('users')
+//     .get()
+//     .then(users => {
+//       // get all zipCodes from the users in a set to avoid duplicates
+//       const zipCodesSet = new Set()
 
-      users.forEach(user => {
-        zipCodesSet.add(user.data().zip)
-      })
+//       users.forEach(user => {
+//         zipCodesSet.add(user.data().zip)
+//       })
 
-      //convert set to an array
-      const zipCodes = Array.from(zipCodesSet)
+//       //convert set to an array
+//       const zipCodes = Array.from(zipCodesSet)
 
-      // Get locations array of object location
-      const locations = []
-      zipCodes.forEach(zip => {
-        const location = zipcodes.lookup(zip)
-        if (location) locations.push(location)
-      })
+//       // Get locations array of object location
+//       const locations = []
+//       zipCodes.forEach(zip => {
+//         const location = zipcodes.lookup(zip)
+//         if (location) locations.push(location)
+//       })
 
-      // Write each location to db
-      Promise.each(locations, writeWeather)
-        .then(() => {
-          response.json('Writing to DB, check logs')
-        })
-        .catch(console.error)
-    })
+//       // Write each location to db
+//       Promise.each(locations, writeWeather)
+//         .then(() => {
+//           response.json('Writing to DB, check logs')
+//         })
+//         .catch(console.error)
+//     })
 
-  function writeWeather(location) {
-    forecast.get([location.latitude, location.longitude], function(err, weather) {
-      if (err) return console.dir(err)
+//   function writeWeather(location) {
+//     forecast.get([location.latitude, location.longitude], function(err, weather) {
+//       if (err) return console.dir(err)
 
-      const date = dateMaker()
-      return admin
-        .firestore()
-        .collection('weather')
-        .doc('days')
-        .collection(date)
-        .doc('zip')
-        .collection(location.zip)
-        .doc('forecast')
-        .set(weather.daily.data[0])
-        .then(() => {
-          console.log('wrote weather for', location.zip)
-        })
-        .catch(console.error.bind(console))
-    })
-  }
-})
+//       const date = dateMaker()
+//       return admin
+//         .firestore()
+//         .collection('weather')
+//         .doc('days')
+//         .collection(date)
+//         .doc('zip')
+//         .collection(location.zip)
+//         .doc('forecast')
+//         .set(weather.daily.data[0])
+//         .then(() => {
+//           console.log('wrote weather for', location.zip)
+//         })
+//         .catch(console.error.bind(console))
+//     })
+//   }
+// })
