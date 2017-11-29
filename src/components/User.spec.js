cd74fbd0bf974be8bcb20483d24f828941cfd2a9
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-11-28 19:02:48 -0500

    test for user time select component

---
+++ src/components/User.spec.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import React from 'react'
+import enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { User } from './User'
+
+const adapter = new Adapter()
+enzyme.configure({ adapter })
+describe('User', () => {
+  let user
+  beforeEach(() => {
+    user = shallow(<User email={'joe@email.com'} />)
+  })
+
+  it('renders the email in h2', () => {
+    expect(user.find('h2'.text()).to.be.equal('Welcome, joe@email.com'))
+  })
+})
