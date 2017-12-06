a48f2a2b3c43a6c0a60074b6b005525118dac147
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-12-06 12:00:29 -0500

    add user test and weather test, travis

---
+++ src/User.spec.js
@@ -0,0 +1,21 @@
+/* global describe beforeEach it */
+
+import { expect } from 'chai'
+import React from 'react'
+import enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { User } from './user-home'
+
+const adapter = new Adapter()
+enzyme.configure({ adapter })
+describe('User Component', () => {
+  let user
+
+  beforeEach(() => {
+    user = shallow(<User email={'cody@email.com'} />)
+  })
+
+  it('renders the email in an hw', () => {
+    expect(user.find('hw').text()).to.be.equal('Welcome, cody@email.com')
+  })
+})
--- src/components/WeatherItem.spec.js
+++ src/components/WeatherItem.spec.js
@@ -4,7 +4,7 @@ import { expect } from 'chai'
 import React from 'react'
 import enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { WeatherItem } from './WeatherItem'
+import WeatherItem from './WeatherItem'
 
 const adapter = new Adapter()
 enzyme.configure({ adapter })
