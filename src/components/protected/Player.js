78517ca967fe3eccd238ee7f0ea47faa9f454edc
Adrien <verblodung@gmail.com>
2017-12-09 18:11:47 -0500

    feat(player): removing unecessary buttons

--- src/components/protected/Player.js
+++ src/components/protected/Player.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react'
-import FontIcon from 'material-ui/FontIcon'
 import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation'
 import Paper from 'material-ui/Paper'
-import IconLocationOn from 'material-ui/svg-icons/communication/location-on'
+import PlayIcon from 'material-ui/svg-icons/av/play-arrow'
 
-const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>
-const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>
-const nearbyIcon = <IconLocationOn />
+const playIcon = <PlayIcon />
 
-/**
- * A simple example of `BottomNavigation`, with three labels and icons
- * provided. The selected `BottomNavigationItem` is determined by application
- * state (for instance, by the URL).
- */
 class Player extends Component {
   state = {
-    selectedIndex: 0
+    selectedIndex: 2
   }
 
   select = index => this.setState({ selectedIndex: index })
@@ -24,13 +16,17 @@ class Player extends Component {
     return (
       <Paper zDepth={1}>
         <BottomNavigation selectedIndex={this.state.selectedIndex}>
-          <BottomNavigationItem label="Recents" icon={recentsIcon} onClick={() => this.select(0)} />
+          {/* <BottomNavigationItem label="Recents" icon={recentsIcon} onClick={() => this.select(0)} />
           <BottomNavigationItem
             label="Favorites"
             icon={favoritesIcon}
             onClick={() => this.select(1)}
+          /> */}
+          <BottomNavigationItem
+            label="Play your daily summary"
+            icon={playIcon}
+            onClick={() => this.select(2)}
           />
-          <BottomNavigationItem label="Nearby" icon={nearbyIcon} onClick={() => this.select(2)} />
         </BottomNavigation>
       </Paper>
     )
